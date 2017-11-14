582730ec32e32dccd0e457552fb8911adace4305
Manuel Schneider <manuelschneid3r@gmail.com>
2017-11-14 20:14:04 +0100

    Polish pacman

--- Pacman.py
+++ Pacman.py
@@ -1,5 +1,4 @@
-"""
-Extension for the package manager `pacman`
+"""Extension for the package manager `pacman`
 
 The extension provides a way to install, remove and search for packages in the
 archlinux.org database. To trigger the extension you just need to type `pacman `
@@ -10,104 +9,74 @@ Otherwise albert will try to search for packages with the search query within
 the package name.
 
 For more information about `pacman` please have a look at:
-    - https://wiki.archlinux.org/index.php/pacman
-"""
+    - https://wiki.archlinux.org/index.php/pacman"""
 
-import os
-import subprocess
-import re
 from albertv0 import *
 from shutil import which
+import subprocess
 
 __iid__ = "PythonInterface/v0.1"
-__prettyname__ = "pacman"
-__version__ = "1.0"
+__prettyname__ = "PacMan"
+__version__ = "1.1"
 __trigger__ = "pacman "
-__author__ = "Benedict Dudel"
-__dependencies__ = ["pacman"]
+__author__ = "Manuel Schneider, Benedict Dudel"
+__dependencies__ = ["pacman", "expac"]
 
 
 if which("pacman") is None:
     raise Exception("'pacman' is not in $PATH.")
 
-iconPath = "system-software-install"
+iconPath = iconLookup("system-software-install")
 
 
 def handleQuery(query):
     if query.isTriggered:
         if not query.string.strip():
             return [Item(
-                id = __prettyname__,
-                icon = iconPath,
-                text = "Update all packages on the system",
-                subtext = "Synchronizes the repository databases and updates the system's packages",
-                completion = __trigger__,
-                actions = [
-                    TermAction(
-                        text = "Update all packages",
-                        commandline = ["pacman", "-Syu"]
-                    ),
-                ]
+                id="%s-update" % __prettyname__,
+                icon=iconPath,
+                text="Update all packages on the system",
+                subtext="Synchronizes the repository databases and updates the system's packages",
+                completion=__trigger__,
+                actions=[TermAction("Update the system", ["sudo", "pacman", "-Syu"])]
             )]
 
-        packages = getPackages(query.string.strip())
         items = []
-        for package in packages:
+        proc = subprocess.Popen(["expac", "-H", "M", "-Ss", "%n\n%v\n%r\n%d\n%u", query.string.strip()],
+                                stdout=subprocess.PIPE)
+        for line in proc.stdout:
+            name = line.decode().rstrip()
+            vers = proc.stdout.readline().decode().rstrip()
+            repo = proc.stdout.readline().decode().rstrip()
+            desc = proc.stdout.readline().decode().rstrip()
+            purl = proc.stdout.readline().decode().rstrip()
+
             items.append(Item(
-                id = package["package"],
-                icon = iconPath,
-                text = package["name"],
-                subtext = package["package"],
-                completion = "%s%s" % (__trigger__, package["name"]),
-                actions = [
-                    TermAction(
-                        text = "Install",
-                        commandline = ["pacman", "-S", package["name"]]
-                    ),
-                    TermAction(
-                        text = "Remove",
-                        commandline = ["pacman", "-Rs", package["name"]]
-                    ),
-                    UrlAction(
-                        text = "Search on archlinux.org",
-                        url = "https://www.archlinux.org/packages/?q=%s" % package["name"]
-                    )
+                id="%s%s%s" % (__prettyname__, repo, name),
+                icon=iconPath,
+                text="%s %s" % (name, vers),
+                subtext="[%s] %s" % (repo, desc),
+                completion="%s%s" % (query.trigger, name),
+                actions=[
+                    TermAction("Install", ["sudo", "pacman", "-S", name]),
+                    TermAction("Remove", ["sudo", "pacman", "-Rs", name]),
+                    UrlAction("Show on packages.archlinux.org",
+                              "https://www.archlinux.org/packages/%s/x86_64/%s/" % (repo, name)),
+                    UrlAction("Show project website", purl)
                 ]
             ))
 
-        if len(items) <= 0:
+        if not items:
             return [Item(
-                id = __prettyname__,
-                icon = iconPath,
-                text = "Search on archlinux.org",
-                subtext = "No results found in the local database",
-                completion = __trigger__,
-                actions = [
-                    UrlAction(
-                        text = "Search on archlinux.org",
-                        url = "https://www.archlinux.org/packages/?q=%s" % query.string.strip()
-                    )
+                id="%s-empty" % __prettyname__,
+                icon=iconPath,
+                text="Search on archlinux.org",
+                subtext="No results found in the local database",
+                completion=__trigger__,
+                actions=[
+                    UrlAction("Search on archlinux.org",
+                              "https://www.archlinux.org/packages/?q=%s" % query.string.strip())
                 ]
             )]
 
         return items
-
-def getPackages(query):
-    items = []
-
-    result = subprocess.Popen(["pacman", "-Ss", query], stdout=subprocess.PIPE)
-    lines = result.stdout.readlines()
-
-    line = 0
-    while line < len(lines):
-        result = re.search("/(.*)\s", lines[line].decode("utf-8").strip())
-        if result and result.group(1) and query in result.group(1):
-            items.append({
-                "name": result.group(1),
-                "package": lines[line].decode("utf-8").strip(),
-                "description": lines[line + 1].decode("utf-8").strip()
-            })
-
-        line = line + 2
-
-    return items
