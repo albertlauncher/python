e3287847114273f25f78a7f0a782a6e167bfad5c
Manuel Schneider <manuelschneid3r@gmail.com>
2017-11-15 16:43:42 +0100

    Gnote+Tomboy improvements

--- Gnote.py
+++ Gnote.py
@@ -5,59 +5,70 @@
 Search, open, create and delete notes."""
 
 from albertv0 import *
-from dbus import SessionBus, Interface
+from dbus import SessionBus, Interface, DBusException
 from shutil import which
 from datetime import datetime
+import re
 
 __iid__ = "PythonInterface/v0.1"
 __prettyname__ = "Gnote"
-__version__ = "1.0"
+__version__ = "1.1"
 __trigger__ = "gn "
 __author__ = "Manuel Schneider"
 __bin__ = __prettyname__.lower()
 __dependencies__ = [__bin__, "python-dbus"]
 
+BUS = "org.gnome.%s" % __prettyname__
+OBJ = "/org/gnome/%s/RemoteControl" % __prettyname__
+IFACE = 'org.gnome.%s.RemoteControl' % __prettyname__
+
 if which(__bin__) is None:
     raise Exception("'%s' is not in $PATH." % __bin__)
 
-
-def initialize():
-    global dbus, iconPath
-    obj = SessionBus().get_object("org.gnome.%s" % __prettyname__,
-                                  "/org/gnome/%s/RemoteControl" % __prettyname__)
-    dbus = Interface(obj, dbus_interface='org.gnome.%s.RemoteControl' % __prettyname__)
-    iconPath = iconLookup(__bin__)
+iconPath = iconLookup(__bin__)
 
 
 def handleQuery(query):
     results = []
     if query.isTriggered:
-        if query.string.strip():
-            for note in dbus.SearchNotes(query.string.lower(), False):
-                results.append(Item(id="%s%s" % (__prettyname__, note),
+        try:
+            if not SessionBus().name_has_owner(BUS):
+                warning("Seems like %s is not running" % __bin__)
+                return []
+
+            obj = SessionBus().get_object(bus_name=BUS, object_path=OBJ)
+            iface = Interface(obj, dbus_interface=IFACE)
+
+            if query.string.strip():
+                for note in iface.SearchNotes(query.string.lower(), False):
+                    results.append(
+                        Item(id="%s%s" % (__prettyname__, note),
+                             icon=iconPath,
+                             text=iface.GetNoteTitle(note),
+                             subtext="%s%s" % ("".join(["#%s " % re.search('.+:.+:(.+)', s).group(1) for s in iface.GetTagsForNote(note)]),
+                                               datetime.fromtimestamp(iface.GetNoteChangeDate(note)).strftime("Note from %c")),
+                             completion=query.rawString,
+                             actions=[
+                                 FuncAction("Open note",
+                                            lambda note=note: iface.DisplayNote(note)),
+                                 FuncAction("Delete note",
+                                            lambda note=note: iface.DeleteNote(note))
+                                 ]))
+            else:
+                def createAndShowNote():
+                    note = iface.CreateNote()
+                    iface.DisplayNote(note)
+
+                results.append(Item(id="%s-create" % __prettyname__,
                                     icon=iconPath,
-                                    text=dbus.GetNoteTitle(note),
-                                    subtext=datetime.fromtimestamp(dbus.GetNoteChangeDate(note)).strftime("Note from %c"),
+                                    text=__prettyname__,
+                                    subtext="%s notes" % __prettyname__,
                                     completion=query.rawString,
                                     actions=[
-                                        FuncAction("Open note",
-                                                   lambda note=note: dbus.DisplayNote(note)),
-                                        FuncAction("Delete note",
-                                                   lambda note=note: dbus.DeleteNote(note))
+                                        FuncAction("Open %s" % __prettyname__,
+                                                   lambda: iface.DisplaySearch()),
+                                        FuncAction("Create a new note", createAndShowNote)
                                     ]))
-        else:
-            def createAndShowNote():
-                note = dbus.CreateNote()
-                dbus.DisplayNote(note)
-
-            results.append(Item(id="%s-create" % __prettyname__,
-                                icon=iconPath,
-                                text=__prettyname__,
-                                subtext="%s notes" % __prettyname__,
-                                completion=query.rawString,
-                                actions=[
-                                    FuncAction("Open %s" % __prettyname__,
-                                               lambda: dbus.DisplaySearch()),
-                                    FuncAction("Create a new note", createAndShowNote)
-                                ]))
+        except DBusException as e:
+            critical(str(e))
     return results
