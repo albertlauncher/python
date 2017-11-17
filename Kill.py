a38ad9ae45fc2059b51514a54d39fbfd08163e35
Manuel Schneider <manuelschneid3r@gmail.com>
2017-11-17 15:41:56 +0100

    Use scan dir

--- Kill.py
+++ Kill.py
@@ -5,7 +5,7 @@ from albertv0 import *
 
 __iid__ = "PythonInterface/v0.1"
 __prettyname__ = "Kill Process"
-__version__ = "1.1"
+__version__ = "1.2"
 __trigger__ = "kill "
 __author__ = "Benedict Dudel, Manuel Schneider"
 __dependencies__ = []
@@ -23,24 +23,25 @@ def handleQuery(query):
     if query.isTriggered:
         results = []
         uid = os.getuid()
-        for pid in (pid for pid in os.listdir('/proc') if pid.isdigit() and owner(pid) == uid):
-            try:
-                proc_command = open(os.path.join('/proc', pid, 'comm'), 'r').read().strip()
-                if query.string in proc_command:
-                    proc_cmdline = open(os.path.join('/proc', pid, 'cmdline'), 'r').read().strip().replace("\0", " ")
-                    results.append(
-                        Item(
-                            id="kill_%s" % pid,
-                            icon=iconPath,
-                            text=proc_command,
-                            subtext=proc_cmdline,
-                            completion=query.rawString,
-                            actions=[
-                                FuncAction("Terminate", lambda pid=pid: os.kill(pid, os.SIGTERM)),
-                                FuncAction("Kill", lambda pid=pid: os.kill(pid, os.SIGKILL))
-                            ]
+        for dir_entry in os.scandir('/proc'):
+            if dir_entry.name.isdigit() and dir_entry.stat().st_uid == uid:
+                try:
+                    proc_command = open(os.path.join(dir_entry.path, 'comm'), 'r').read().strip()
+                    if query.string in proc_command:
+                        proc_cmdline = open(os.path.join(dir_entry.path, 'cmdline'), 'r').read().strip().replace("\0", " ")
+                        results.append(
+                            Item(
+                                id="kill_%s" % dir_entry.name,
+                                icon=iconPath,
+                                text=proc_command,
+                                subtext=proc_cmdline,
+                                completion=query.rawString,
+                                actions=[
+                                    FuncAction("Terminate", lambda pid=dir_entry.name: os.kill(pid, os.SIGTERM)),
+                                    FuncAction("Kill", lambda pid=dir_entry.name: os.kill(pid, os.SIGKILL))
+                                ]
+                            )
                         )
-                    )
-            except IOError:
-                continue
+                except IOError:  # TOCTOU dirs may disappear
+                    continue
         return results
