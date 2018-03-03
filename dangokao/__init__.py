70b4e1f79ebe2b3d9194de9d63c75c57dda0c250
David Britt <djmb102@gmail.com>
2018-03-03 22:49:53 +0100

    Fixed indentation and added score.

--- dangokao/__init__.py
+++ dangokao/__init__.py
@@ -7,8 +7,6 @@ Find relevant Kaomoji's using machine learning.
 from albertv0 import *
 import os
 import json
-import urllib.request
-import urllib.parse
 import urllib.error
 import requests
 
@@ -22,40 +20,40 @@ __dependencies__ = []
 icon_path = os.path.dirname(__file__) + "/kaoicon.svg"
 dangoUrl = "https://customer.getdango.com/dango/api/query/kaomoji"
 
+
 def handleQuery(query):
-	results = []
+    results = []
 
-	if query.isTriggered:
+    if query.isTriggered:
 
-		item = Item(
+        item = Item(
             id=__prettyname__,
             icon=icon_path,
             completion=query.rawString,
             text=__prettyname__,
             actions=[]
-		)
+        )
 
-		if len(query.string) >= 2:
-			try:
-				with requests.get(dangoUrl, params={"q": query.string}) as api_response:
-					json_data = json.loads(api_response.text)
-					for emoj in json_data["items"]:
-						results.append(
-							Item(
-								    id=__prettyname__,
-                                    icon=icon_path,
-                                    text=emoj["text"],
-                                    actions=[
-                                        ClipAction("Copy translation to clipboard", emoj["text"])
-                                    ]
-							)
-						)
-			except urllib.error.URLError as urlerr:
-				print("Troubleshoot internet connection: %s" % urlerr)
-				item.subtext = "Connection error"
-				return item
-		else:
-			item.subtext = "Search emojis!"
-			return item
+        if len(query.string) >= 2:
+            try:
+                with requests.get(dangoUrl, params={"q": query.string}) as api_response:
+                    json_data = json.loads(api_response.text)
+                    for emoj in json_data["items"]:
+                        results.append(Item(
+                            id=__prettyname__,
+                            icon=icon_path,
+                            text=emoj["text"],
+                            actions=[
+                                ClipAction(
+                                    "Copy translation to clipboard", emoj["text"])
+                            ]
+                        ))
+            except urllib.error.URLError as urlerr:
+                print("Troubleshoot internet connection: %s" % urlerr)
+                item.subtext = "Connection error"
+                return item
+        else:
+            item.subtext = "Search emojis!"
+            return item
 
-	return results
+    return results
