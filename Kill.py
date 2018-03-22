08b64f0d12a0d293f067f696811c81e768bb3023
Manuel Schneider <manuelschneid3r@gmail.com>
2018-03-22 15:33:14 +0100

    [kill] Catch FileNotFound

--- Kill.py
+++ Kill.py
@@ -6,7 +6,7 @@ from albertv0 import *
 
 __iid__ = "PythonInterface/v0.1"
 __prettyname__ = "Kill Process"
-__version__ = "1.2"
+__version__ = "1.3"
 __trigger__ = "kill "
 __author__ = "Benedict Dudel, Manuel Schneider"
 __dependencies__ = []
@@ -18,25 +18,28 @@ def handleQuery(query):
     if query.isTriggered:
         results = []
         uid = os.getuid()
-        for dir_entry in os.scandir('/proc'):
-            if dir_entry.name.isdigit() and dir_entry.stat().st_uid == uid:
+        with os.scandir('/proc') as it:
+            for dir_entry in it:
                 try:
-                    proc_command = open(os.path.join(dir_entry.path, 'comm'), 'r').read().strip()
-                    if query.string in proc_command:
-                        proc_cmdline = open(os.path.join(dir_entry.path, 'cmdline'), 'r').read().strip().replace("\0", " ")
-                        results.append(
-                            Item(
-                                id="kill_%s" % dir_entry.name,
-                                icon=iconPath,
-                                text=proc_command.replace(query.string, "<u>%s</u>" % query.string),
-                                subtext=proc_cmdline,
-                                completion=query.rawString,
-                                actions=[
-                                    FuncAction("Terminate", lambda pid=int(dir_entry.name): os.kill(pid, SIGTERM)),
-                                    FuncAction("Kill", lambda pid=int(dir_entry.name): os.kill(pid, SIGKILL))
-                                ]
+                    if dir_entry.name.isdigit() and dir_entry.stat().st_uid == uid:
+                        proc_command = open(os.path.join(dir_entry.path, 'comm'), 'r').read().strip()
+                        if query.string in proc_command:
+                            proc_cmdline = open(os.path.join(dir_entry.path, 'cmdline'), 'r').read().strip().replace("\0", " ")
+                            results.append(
+                                Item(
+                                    id="kill_%s" % dir_entry.name,
+                                    icon=iconPath,
+                                    text=proc_command.replace(query.string, "<u>%s</u>" % query.string),
+                                    subtext=proc_cmdline,
+                                    completion=query.rawString,
+                                    actions=[
+                                        FuncAction("Terminate", lambda pid=int(dir_entry.name): os.kill(pid, SIGTERM)),
+                                        FuncAction("Kill", lambda pid=int(dir_entry.name): os.kill(pid, SIGKILL))
+                                    ]
+                                )
                             )
-                        )
+                except FileNotFoundError:  # TOCTOU dirs may disappear
+                    continue
                 except IOError:  # TOCTOU dirs may disappear
                     continue
         return results
