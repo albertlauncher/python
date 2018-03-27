19f2411a1ee058a881307452740008f217c38778
Manuel Schneider <manuelschneid3r@gmail.com>
2018-03-27 13:42:52 +0200

    Add datetime extension
    Supersedes "Epoch"

---
+++ DateTime.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+"""Retrieve and convert datetime strings.
+
+This extension provides items for 'time', 'date', 'datetime' and 'epoch' respectively 'unixtime'.
+The latter two yield the unix timestamp and also accept a unix timestamp as parameter which will be
+converted to a datetime string."""
+
+from albertv0 import *
+import datetime
+import time
+
+__iid__ = "PythonInterface/v0.1"
+__prettyname__ = "DateTime"
+__version__ = "1.0"
+__trigger__ = None
+__author__ = "Manuel Schneider"
+__dependencies__ = []
+
+iconPath = iconLookup('x-office-calendar')
+
+
+def handleQuery(query):
+    fields = list(filter(None, query.string.split()))
+    if fields:
+        def makeItem(text: str, subtext: str):
+            return Item(
+                id=__prettyname__,
+                icon=iconPath,
+                text=text,
+                subtext=subtext,
+                completion=query.rawString,
+                actions=[ClipAction("Copy to clipboard", text)]
+            )
+
+        if "date".startswith(fields[0]) and len(fields) == 1:
+            return makeItem(datetime.date.today().strftime("%x"),
+                            "Current date")
+        elif "time".startswith(fields[0]) and len(fields) == 1:
+            return makeItem(datetime.datetime.now().strftime("%X"),
+                            "Current time")
+        elif "datetime".startswith(fields[0]) and len(fields) == 1:
+            return makeItem(datetime.datetime.now().strftime("%c"),
+                            "Current date and time")
+        elif "unixtime".startswith(fields[0]) or "epoch".startswith(fields[0]):
+            if len(fields) == 2:
+                if fields[1].isdigit():
+                    return makeItem(time.strftime("%c", time.localtime(int(fields[1]))),
+                                    "Date and time of '%s'" % fields[1])
+                else:
+                    return Item(
+                        id=__prettyname__,
+                        icon=iconPath,
+                        text="Invalid input",
+                        subtext="Argument must be empty or numeric.",
+                        completion=query.rawString
+                    )
+            else:
+                return makeItem(datetime.datetime.now().strftime("%s"),
+                                "Current unixtime")
