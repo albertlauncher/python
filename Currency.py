95132758c2a42f1e5917199e69ee5828da39d599
Gordon Walsh <gordon.p.walsh@gmail.com>
2018-05-02 13:21:03 +0100

    Add ability to use fallback providers. Yahoo Finance and then XE.com.

--- Currency.py
+++ Currency.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-"""Convert currencies using Google Finance.
+"""Convert currencies using Yahoo Finance and XE.com.
 Usage: exch <amount> <src currency> <dest currency>
 Example: exch 5 usd eur"""
 
 from albertv0 import *
+import urllib
 from urllib.request import urlopen
 import re
 
@@ -25,20 +26,35 @@ def handleQuery(query):
         fields = query.string.split()
         item = Item(id=__prettyname__, icon=iconPath, completion=query.rawString)
         if len(fields) == 3:
-            url = 'https://search.yahoo.com/search?p=%s+%s+to+%s' % tuple(fields)
-            with urlopen(url) as response:
+            urlGo = 'https://finance.google.com/finance/converter?a=%s&from=%s&to=%s' % tuple(fields)
+            urlXE = 'https://www.xe.com/currencyconverter/convert/?Amount=%s&From=%s&To=%s' % tuple(fields)
+            urlYa = 'https://search.yahoo.com/search?p=%s+%s+to+%s' % tuple(fields)
+            urls = [urlYa, urlXE, urlGo]
+
+            # Dictionary of URLs and their Regex expressions in the html.
+            reDict = {
+                urlGo: '<div id=currency_converter_result>.*<span class=bld>(\d+\.\d+).*</span>',
+                urlXE: '<span class=\'uccResultAmount\'>(\d+\.\d+).*</span>',
+                urlYa: '<span class=.*convert-to.*>(\d+(\.\d+)?)'
+                }
+
+            # Loop over the urls in the order of the list.
+            # First one that doesn't except will return the function.
+            for url in urls:
+                try:
+                    response = urlopen(url)
+                except urllib.error.HTTPError as e:
+                    response = e
+
                 html = response.read().decode("latin-1")
-                m = re.search('<span class=.*convert-to.*>(\d+(\.\d+)?)', html)
+                m = re.search(reDict[url], html)
                 if m:
                     result = m.group(1)
                     item.text = result
                     item.subtext = "Value of %s %s in %s" % tuple([x.upper() for x in fields])
                     item.addAction(ClipAction("Copy result to clipboard", result))
                     return item
-                else:
-                    item.text = "Error: HTTP reply does not contain a result"
-                    item.subtext = "Maybe Google Finance changed their website"
-                    return item
+
         else:
             item.text = __prettyname__
             item.subtext = "Enter a query in the form of &lt;amount&gt; &lt;from&gt; &lt;to&gt;"
