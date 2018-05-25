0b6bc9e2f264f3b2bf24333a48241aaa2cbe1877
Manuel Schneider <manuelschneid3r@gmail.com>
2018-05-25 13:50:35 +0200

    [timer] More elaborate description

--- Timer/__init__.py
+++ Timer/__init__.py
@@ -3,7 +3,10 @@
 """Set up timers.
 
 Lists all timers when triggered. Additional arguments in the form of [[hours:]minutes:]seconds let
-you set triggers."""
+you set triggers. Empty field resolve to 0, e.g. "96::" starts a 96 hours timer. Fields exceeding
+the maximum amount of the time interval are automatically refactorized, e.g. "9:120:3600" resolves
+to 12 hours.
+"""
 
 from albertv0 import *
 from threading import Timer
