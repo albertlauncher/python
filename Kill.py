cf1a837454c2cfc875392430b8c24e439c1f2620
Manuel Schneider <manuelschneid3r@gmail.com>
2018-06-04 15:28:42 +0200

    [Kill] Make Python3.5 backward compatible
    Closes albertlauncher/albert#669

--- Kill.py
+++ Kill.py
@@ -2,6 +2,7 @@
 
 import os
 from signal import SIGKILL, SIGTERM
+
 from albertv0 import *
 
 __iid__ = "PythonInterface/v0.1"
@@ -18,28 +19,27 @@ def handleQuery(query):
     if query.isTriggered:
         results = []
         uid = os.getuid()
-        with os.scandir('/proc') as it:
-            for dir_entry in it:
-                try:
-                    if dir_entry.name.isdigit() and dir_entry.stat().st_uid == uid:
-                        proc_command = open(os.path.join(dir_entry.path, 'comm'), 'r').read().strip()
-                        if query.string in proc_command:
-                            proc_cmdline = open(os.path.join(dir_entry.path, 'cmdline'), 'r').read().strip().replace("\0", " ")
-                            results.append(
-                                Item(
-                                    id="kill_%s" % dir_entry.name,
-                                    icon=iconPath,
-                                    text=proc_command.replace(query.string, "<u>%s</u>" % query.string),
-                                    subtext=proc_cmdline,
-                                    completion=query.rawString,
-                                    actions=[
-                                        FuncAction("Terminate", lambda pid=int(dir_entry.name): os.kill(pid, SIGTERM)),
-                                        FuncAction("Kill", lambda pid=int(dir_entry.name): os.kill(pid, SIGKILL))
-                                    ]
-                                )
+        for dir_entry in os.scandir('/proc'):
+            try:
+                if dir_entry.name.isdigit() and dir_entry.stat().st_uid == uid:
+                    proc_command = open(os.path.join(dir_entry.path, 'comm'), 'r').read().strip()
+                    if query.string in proc_command:
+                        proc_cmdline = open(os.path.join(dir_entry.path, 'cmdline'), 'r').read().strip().replace("\0", " ")
+                        results.append(
+                            Item(
+                                id="kill_%s" % dir_entry.name,
+                                icon=iconPath,
+                                text=proc_command.replace(query.string, "<u>%s</u>" % query.string),
+                                subtext=proc_cmdline,
+                                completion=query.rawString,
+                                actions=[
+                                    FuncAction("Terminate", lambda pid=int(dir_entry.name): os.kill(pid, SIGTERM)),
+                                    FuncAction("Kill", lambda pid=int(dir_entry.name): os.kill(pid, SIGKILL))
+                                ]
                             )
-                except FileNotFoundError:  # TOCTOU dirs may disappear
-                    continue
-                except IOError:  # TOCTOU dirs may disappear
-                    continue
+                        )
+            except FileNotFoundError:  # TOCTOU dirs may disappear
+                continue
+            except IOError:  # TOCTOU dirs may disappear
+                continue
         return results
