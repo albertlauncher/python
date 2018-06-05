04a85993d9e44e5d021fd90a37eff5bf6744713b
Manuel Schneider <manuelschneid3r@gmail.com>
2018-06-05 14:14:30 +0200

    [Units] Add triggerless infix handling
    E.g. barrel to decimeter^3;inch^3

--- Units.py
+++ Units.py
@@ -1,16 +1,27 @@
 # -*- coding: utf-8 -*-
 
-"""This extension is an adaptor for the powerful GNU units.
-Synopsis: 'units <from> [to]'
-Note that spaces are separators."""
+"""This extension is a wrapper for the (extremely) powerful GNU units tool. \
+Note that spaces are separators.
 
-from albertv0 import *
+Synopsis: units <input>
+Synopsis: <from> to <to>
+
+Examples:
+
+mach to km/hr
+0.7day to hour;min
+barrel to decimeter^3;inch^3
+"""
+
+import re
 import subprocess as sp
 from shutil import which
 
+from albertv0 import *
+
 __iid__ = "PythonInterface/v0.1"
 __prettyname__ = "GNU Units"
-__version__ = "1.0"
+__version__ = "1.1"
 __trigger__ = "units "
 __author__ = "Manuel Schneider"
 __dependencies__ = ["units"]
@@ -22,23 +33,35 @@ icon = iconLookup('calc')
 if not icon:
     icon = ":python_module"
 
+regex = re.compile(r"(\S+)(?:\s+to)?\s+(\S+)")
+strict_regex = re.compile(r"(\S+)(?:\s+to)\s+(\S+)")
 
 def handleQuery(query):
+
     if query.isTriggered:
         args = query.string.split()
         item = Item(id='python.gnu_units', icon=icon, completion=query.rawString)
-        if len(args) < 1:
-            item.text = 'Enter something to convert'
-            item.subtext = 'Units takes one or two arguments.'
-        elif len(args) > 2:
-            item.text = 'Too many arguments'
-            item.subtext = 'Units takes one or two arguments.'
-        else:
+        if args:
             try:
-                item.text = sp.check_output(['units', '-t'] + args,
-                                            stderr=sp.STDOUT).decode('utf-8').strip()
+                item.text = sp.check_output(['units', '-t'] + query.string.split(), stderr=sp.STDOUT).decode().strip()
+                item.addAction(ClipAction("Copy to clipboard", item.text))
             except sp.CalledProcessError as e:
-                item.text = e.stdout.decode('utf-8').strip().partition('\n')[0]
+                item.text = e.stdout.decode().strip().partition('\n')[0]
             item.subtext = "Result of 'units -t %s'" % query.string
-            item.addAction(ClipAction("Copy to clipboard", item.text))
+        else:
+            item.text = "Empty input"
+            item.subtext = "Enter something to convert"
         return item
+
+    else:
+        match = strict_regex.match(query.string)
+        if match:
+            args = match.group(1, 2)
+            try:
+                item = Item(id='python.gnu_units', icon=icon, completion=query.rawString)
+                item.text = sp.check_output(['units', '-t'] + list(args)).decode().strip()
+                item.subtext = "Result of 'units -t %s %s'" % args
+                item.addAction(ClipAction("Copy to clipboard", item.text))
+                return item
+            except sp.CalledProcessError as e:
+                pass
