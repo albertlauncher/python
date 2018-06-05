83bf1c3d72fa5da09d633557e73a053bde6f7eaa
Manuel Schneider <manuelschneid3r@gmail.com>
2018-06-05 11:55:09 +0200

    [currency] Overhaul. See details…
    * Use multiple providers
    * Add ECB provider
    * Remove trigger

--- Currency.py
+++ Currency.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 
-"""Convert currencies using Google Finance.
-Usage: exch <amount> <src currency> <dest currency>
+"""Convert currencies.
+
+Synopsis: <amount> <src currency> [to|as|in] <dest currency>
+
 Example: exch 5 usd eur"""
 
-from albertv0 import *
-from urllib.request import urlopen
 import re
+import time
+from urllib.request import urlopen
+from xml.etree import ElementTree
+
+from albertv0 import *
 
 __iid__ = "PythonInterface/v0.1"
 __prettyname__ = "Currency converter"
 __version__ = "1.0"
-__trigger__ = "exch "
 __author__ = "Manuel Schneider"
 __dependencies__ = []
 
@@ -20,26 +24,62 @@ if not iconPath:
     iconPath = ":python_module"
 
 
+class EuropeanCentralBank:
+
+    url = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
+
+    def __init__(self):
+        self.lastUpdate = 0
+        self.exchange_rates = dict()
+        self.name = "European Central Bank"
+
+    def convert(self, amount, src, dst):
+        if self.lastUpdate < time.time()-10800:  # Update every 3 hours
+            self.exchange_rates.clear()
+            with urlopen(EuropeanCentralBank.url) as response:
+                tree = ElementTree.fromstring(response.read().decode())
+                for child in tree[2][0]:
+                    curr = child.attrib['currency']
+                    rate = float(child.attrib['rate'])
+                    self.exchange_rates[curr] = rate
+                self.exchange_rates["EUR"] = 1.0  # For simpler algorithmic
+                info("%s: Updated foreign exchange rates." % __prettyname__)
+                debug(str(self.exchange_rates))
+            self.lastUpdate = time.time()
+
+        if src in self.exchange_rates and dst in self.exchange_rates:
+            src_rate = self.exchange_rates[src]
+            dst_rate = self.exchange_rates[dst]
+            return str(amount / src_rate * dst_rate)
+
+class Yahoo:
+
+    def __init__(self):
+        self.name = "Yahoo"
+
+    def convert(self, amount, src, dst):
+        url = 'https://search.yahoo.com/search?p=%s+%s+to+%s' % (amount, src, dst)
+        with urlopen(url) as response:
+            html = response.read().decode()
+            m = re.search('<span class=.*convert-to.*>(\d+(\.\d+)?)', html)
+            if m:
+                return m.group(1)
+
+
+providers = [EuropeanCentralBank(), Yahoo()]
+regex = re.compile(r"(\d+\.?\d*)\s+(\w{3})(?:\s+(?:to|in|as))?\s+(\w{3})")
+
 def handleQuery(query):
-    if query.isTriggered:
-        fields = query.string.split()
+    match = regex.match(query.string)
+    if match:
+        prep = (float(match.group(1)), match.group(2).upper(), match.group(3).upper())
         item = Item(id=__prettyname__, icon=iconPath, completion=query.rawString)
-        if len(fields) == 3:
-            url = 'https://search.yahoo.com/search?p=%s+%s+to+%s' % tuple(fields)
-            with urlopen(url) as response:
-                html = response.read().decode("latin-1")
-                m = re.search('<span class=.*convert-to.*>(\d+(\.\d+)?)', html)
-                if m:
-                    result = m.group(1)
-                    item.text = result
-                    item.subtext = "Value of %s %s in %s" % tuple([x.upper() for x in fields])
-                    item.addAction(ClipAction("Copy result to clipboard", result))
-                    return item
-                else:
-                    item.text = "Error: HTTP reply does not contain a result"
-                    item.subtext = "Maybe Google Finance changed their website"
-                    return item
+        for provider in providers:
+            result = provider.convert(*prep)
+            if result:
+                item.text = result
+                item.subtext = "Value of %s %s in %s (Source: %s)" % (*prep, provider.name)
+                item.addAction(ClipAction("Copy result to clipboard", result))
+                return item
         else:
-            item.text = __prettyname__
-            item.subtext = "Enter a query in the form of &lt;amount&gt; &lt;from&gt; &lt;to&gt;"
-            return item
+            warning("None of the foreign exchange rate providers came up with a result for %s" % str(prep))
