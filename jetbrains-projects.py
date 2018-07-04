a71460ca797cb34ef013c9c1cbd4e7579b0f5e9f
Matthew Bogner <matt@ibogner.net>
2018-07-04 14:54:28 -0500

    look for plain 'idea' command on the path

--- jetbrains-projects.py
+++ jetbrains-projects.py
@@ -19,7 +19,7 @@ paths = [#<Name for config directory>, <possible names for the binary>, <possibl
     ["CLion", "clion", "jetbrains-clion"],
     ["DataGrip", "datagrip", "jetbrains-datagrip"],
     ["GoLand", "goland", "jetbrains-goland"],
-    ["IntelliJIdea", "intellij-idea-ue-bundled-jre intellij-idea-ultimate-edition idea-ce-eap idea-ue-eap",
+    ["IntelliJIdea", "intellij-idea-ue-bundled-jre intellij-idea-ultimate-edition idea-ce-eap idea-ue-eap idea",
      "jetbrains-idea"],
     ["PhpStorm", "phpstorm", "jetbrains-phpstorm"],
     ["PyCharm", "pycharm pycharm-eap charm", "jetbrains-pycharm"],
