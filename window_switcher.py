112380945aa82d1317c72330b0eadde5f44fce25
Hugo Valk <hugo.valk@randstadgroep.nl>
2019-01-07 13:24:18 +0100

    Support matching in full wm_class and wm_name. Support matching multiple words. This improves switching windows for java applications, which have a wm_class that starts with sun-awt-X11-XFramePeer.FreeMind. Also sometimes the first segment of the wm_class is something generic, like for example main.terminology. Often, the 'nice' application name can be found in the wm_name field.

--- window_switcher.py
+++ window_switcher.py
@@ -14,20 +14,23 @@ Window = namedtuple("Window", ["wid", "desktop", "wm_class", "host", "wm_name"])
 
 __iid__ = "PythonInterface/v0.1"
 __prettyname__ = "Window Switcher"
-__version__ = "1.4"
+__version__ = "1.5"
 __author__ = "Ed Perez, Manuel Schneider"
 __dependencies__ = ["wmctrl"]
 
 if which("wmctrl") is None:
     raise Exception("'wmctrl' is not in $PATH.")
 
+def matchWindow(win, text):
+    return (text in win.wm_class.lower() or text in win.wm_name.lower())
+
 def handleQuery(query):
-    stripped = query.string.strip().lower()
+    stripped = query.string.strip().lower().split()
     if stripped:
         results = []
         for line in subprocess.check_output(['wmctrl', '-l', '-x']).splitlines():
             win = Window(*[token.decode() for token in line.split(None,4)])
-            if win.desktop != "-1"  and stripped in win.wm_class.split('.')[0].lower():
+            if win.desktop != "-1" and all(matchWindow(win, text) for text in stripped):
                 results.append(Item(id="%s%s" % (__prettyname__, win.wm_class),
                                     icon=iconLookup(win.wm_class.split('.')[0]),
                                     text="%s  - <i>Desktop %s</i>" % (win.wm_class.split('.')[-1].replace('-',' '), win.desktop),
