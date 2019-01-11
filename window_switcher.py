9046f897c4b9d5468a230bd0ded20e6b2a567c2f
Hugo Valk <hugo.valk@gmail.com>
2019-01-11 21:22:57 +0100

    Make logic on when a window will be listed as an action more explicit.

--- window_switcher.py
+++ window_switcher.py
@@ -21,16 +21,23 @@ __dependencies__ = ["wmctrl"]
 if which("wmctrl") is None:
     raise Exception("'wmctrl' is not in $PATH.")
 
-def matchWindow(win, text):
-    return (text in win.wm_class.lower() or text in win.wm_name.lower())
+
+def notSticky(win):
+    return win.desktop != "-1"
+
+
+def matchesQuery(win, text):
+    keywords = text.lower().split()
+    return all(keyword in win.wm_class.lower() or keyword in win.wm_name.lower() for keyword in keywords)
+
 
 def handleQuery(query):
-    stripped = query.string.strip().lower().split()
-    if stripped:
+    strippedQuery = query.string.strip()
+    if strippedQuery:
         results = []
         for line in subprocess.check_output(['wmctrl', '-l', '-x']).splitlines():
             win = Window(*[token.decode() for token in line.split(None,4)])
-            if win.desktop != "-1" and all(matchWindow(win, text) for text in stripped):
+            if notSticky(win) and matchesQuery(win, strippedQuery):
                 results.append(Item(id="%s%s" % (__prettyname__, win.wm_class),
                                     icon=iconLookup(win.wm_class.split('.')[0]),
                                     text="%s  - <i>Desktop %s</i>" % (win.wm_class.split('.')[-1].replace('-',' '), win.desktop),
