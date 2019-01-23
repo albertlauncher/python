b1b331c6903a0d719317fb1abb05ddc3da386da7
Manuel Schneider <manuelschneid3r@gmail.com>
2019-01-23 18:22:14 +0100

    [emoji] See details.
    * Render emoji as icons (imagemagick)
    * Worker thread for indexing and icon rendering
    * Use intersection for word results

--- unicode_emoji/__init__.py
+++ unicode_emoji/__init__.py
@@ -1,27 +1,66 @@
 # -*- coding: utf-8 -*-
 
-"""Emoji Picker for Albert
-Usage: :<emoji name>
-Example: :see no evil"""
+"""Offline Unicode emoji picker.
+
+Synopsis: <trigger> [filter]"""
 
 from albertv0 import *
-
+from collections import namedtuple
+from threading import Thread
+import datetime
 import os
 import shutil
+import subprocess
 import urllib.request
-import datetime
 
 __iid__ = "PythonInterface/v0.2"
 __prettyname__ = "Unicode Emojis"
-__version__ = "0.2"
+__version__ = "1.3"
 __trigger__ = ":"
-__author__ = "Tim Zeitz"
-__dependencies__ = []
+__author__ = "Tim Zeitz, Manuel Schneider"
+__dependencies__ = ["convert"]
+
+for dep in __dependencies__:
+    if shutil.which(dep) is None:
+        raise Exception("'%s' is not in $PATH." % dep)
+
+EmojiSpec = namedtuple('EmojiSpec', ['string', 'name', 'modifiers'])
 
 emoji_data_src_url = "https://unicode.org/Public/emoji/latest/emoji-test.txt"
 emoji_data_path = os.path.join(dataLocation(), "emoji.txt")
+icon_path_template = os.path.join(cacheLocation(), __name__, "%s.png")
+emojiSpecs = []
+thread = None
 
-emojis = []
+
+class WorkerThread(Thread):
+    def __init__(self):
+        super().__init__()
+        self.stop = False
+
+    def run(self):
+
+        # Create cache dir
+        cache_dir_path = os.path.join(cacheLocation(), __name__)
+        if not os.path.exists(cache_dir_path):
+            os.mkdir(cache_dir_path)
+
+        # Build the index and icon cache
+        # global emojiSpecs
+        emojiSpecs.clear()
+        with open(emoji_data_path) as f:
+            for line in f:
+                if "; fully-qualified" in line:
+                    emoji, desc = line.split('#', 1)[-1].split(None, 1)
+                    desc = [d.strip().lower() for d in desc.split(':')]
+                    emojiSpecs.append(EmojiSpec(emoji, desc[0], desc[1] if len(desc)==2 else ""))
+
+                    icon_path = icon_path_template % emoji
+                    if not os.path.exists(icon_path):
+                        subprocess.call(["convert", "-pointsize", "64", "-background", "transparent", "pango:%s" % emoji, icon_path])
+
+                if self.stop:
+                    return
 
 def initialize():
     src_directory = os.path.dirname(os.path.realpath(__file__))
@@ -49,7 +88,16 @@ def initialize():
     except Exception as e:
         warn(e)
 
-    build_index()
+    # Build the index and icon cache
+    global thread
+    thread = WorkerThread()
+    thread.start()
+
+def finalize():
+    global thread
+    if thread is not None:
+        thread.stop = True
+        thread.join()
 
 def get_emoji_data_version(path):
     with open(emoji_data_path) as f:
@@ -57,30 +105,16 @@ def get_emoji_data_version(path):
             if "# Date: " in line:
                 return datetime.datetime.strptime(line.strip(), "# Date: %Y-%m-%d, %H:%M:%S GMT")
 
-def build_index():
-    emojis.clear()
-
-    with open(emoji_data_path) as f:
-        for line in f:
-            if "; fully-qualified" in line:
-                [emoji, name] = line.split('#', 1)[-1].split(None, 1)
-                emojis.append((emoji, name.strip(), name.lower().split()))
-
 def handleQuery(query):
     if query.isValid and query.isTriggered:
-        return [Item(id=key, completion=key, icon=emoji, text=emoji, subtext=key, actions=[ClipAction("Copy to clipboard", emoji)]) for (emoji, key, matchcount) in matches(query.string.lower().split())]
-    return []
-
-def matches(needles):
-    matched = [(emoji, name, count_matches(tokens, needles)) for (emoji, name, tokens) in emojis]
-    matched = [(emoji, name, matchcount) for (emoji, name, matchcount) in matched if matchcount > 0]
-    matched = sorted(matched, key=lambda data: data[2], reverse=True)
-    return matched
-
-def count_matches(tokens, needles):
-    count = 0
-    for token in tokens:
-        for needle in needles:
-            if needle in token:
-                count += 1
-    return count
+        items = []
+        query_tokens = query.string.lower().split()
+        # filter emojiSpecs where all query words are in any of the emoji description words
+        for es in filter(lambda e: all(any(n in s for s in [e.name, e.modifiers]) for n in query_tokens), emojiSpecs):
+            items.append(Item(id = "%s%s" % (__name__, es.string),
+                              completion = es.name if not es.modifiers else " ".join([es.name, es.modifiers]),
+                              icon = icon_path_template % es.string,
+                              text = es.name.capitalize(),
+                              subtext = es.modifiers.capitalize() if es.modifiers else "<i>(No modifiers)</i>",
+                              actions = [ClipAction("Copy to clipboard", es.string)]))
+        return items
