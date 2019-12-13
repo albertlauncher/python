aa2a0bc2f0dd0ec898b09b20e37ac2de691d980e
Oğuzcan Küçükbayrak <o.kucukbayrak@gmail.com>
2019-12-13 20:03:55 +0300

    [remmina] Updated with correct plugin

--- remmina/__init__.py
+++ remmina/__init__.py
@@ -1,27 +1,75 @@
 # -*- coding: utf-8 -*-
 
-"""Search Remmina connections."""
+"""Search and start Remmina connections."""
 
 from albertv0 import *
 import os
+import subprocess
+from glob import glob
+from re import search, IGNORECASE
+import configparser
 
-__iid__ = "PythonInterface/v0.1"
+__iid__ = "PythonInterface/v0.2"
 __prettyname__ = "Remmina"
 __version__ = "0.1"
 __trigger__ = "rm"
 __author__ = "Oğuzcan Küçükbayrak"
 __dependencies__ = []
 
-iconPath = "%s/%s.svg" % (os.path.dirname(__file__), __name__)
-connectionsPath = '/home/okb/.local/share/remmina'
+iconPath = "%s/remmina.svg" % (os.path.dirname(__file__))
+connectionsPath = "%s/.local/share/remmina" % (os.environ['HOME'])
+protoIconPath = "/usr/share/icons/hicolor/scalable/emblems/remmina-%s-symbolic.svg"
+
+
+def runRemmina(cf=""):
+    args = (['remmina'], ['remmina', '-c', cf])[len(cf) > 0]
+    subprocess.Popen(
+        args,
+        cwd=os.environ['HOME'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT
+    )
+
+
+def searchConfigFiles(query):
+    results = []
+    files = [f for f in glob(connectionsPath + "**/*.remmina", recursive=True)]
+    for f in files:
+        conf = configparser.ConfigParser()
+        conf.read(f)
+
+        name = conf['remmina']['name']
+        group = conf['remmina']['group']
+        server = conf['remmina']['server']
+        proto = conf['remmina']['protocol']
+        if (search(query, name, IGNORECASE)):
+            results.append(
+                Item(
+                    id=__prettyname__,
+                    icon=protoIconPath % (proto.lower()),
+                    text="%s/ %s" % (group, name),
+                    subtext="%s %s" % (proto, server),
+                    actions=[
+                        FuncAction("Open connection",
+                                   lambda cf=f: runRemmina(cf))
+                    ]
+                )
+            )
+    return results
+
+
 def handleQuery(query):
     if query.isTriggered:
-        return Item(
-                    id=__prettyname__,
-                    icon=iconPath,
-                    text="Remmina",
-                    subtext="This is subtext.",
-                    completion=query.rawString,
-                    actions=[ProcAction("Open Remmina", "remmina")]
-                )
+        stripped = query.string.strip()
+        if stripped:
+            results = searchConfigFiles(stripped)
+            if results:
+                return results
 
+        return Item(
+            id=__prettyname__,
+            icon=iconPath,
+            text=__prettyname__,
+            subtext=__doc__,
+            actions=[FuncAction("Open Remmina", runRemmina)]
+        )
