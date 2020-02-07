e330b57a26cd7535f76ba7dbcbe53eaf56d1f67a
davekch <dave-koch@web.de>
2020-02-08 00:17:36 +0100

    parse linguee response with beautifulsoup and display it in albert

--- linguee/__init__.py
+++ linguee/__init__.py
@@ -9,19 +9,17 @@ Synopsis: <trigger> <word>"""
 
 from albertv0 import *
 import requests
-import xml.etree.ElementTree as ET
-import html
-import urllib
+import bs4
 
 
 lang = "deutsch-englisch"
 
 __iid__ = "PythonInterface/v0.1"
 __prettyname__ = "Linguee-deutsch-englisch"
-__version__ = "1.0"
+__version__ = "0.1"
 __trigger__ = "lin "
-__author__ = "Lucky Lukert"
-__dependencies__ = []
+__author__ = "Lucky Lukert, David Koch"
+__dependencies__ = ["beautifulsoup4"]
 
 iconPath = iconLookup("albert")
 
@@ -37,70 +35,70 @@ def getItem(message):
     )
 
 
-def isbadText(s):
-    badText = ["akk", "adj", "präp", "pron", "n", "nt", "v", "m", "p", "f", "pl", "adv"]
-    if s.lower() in badText:
-        return True
-    if s.startswith("p-") or "/" in s:
-        return True
-    return False
+def str_from_html(html):
+    # extract a meaningful string from a translation_item
+    # the item might be a nested structure, so this function is recursive
+    if type(html) is bs4.element.NavigableString:
+        return html.strip()
+    elif type(html) is bs4.element.Tag:
+        # we are only interested in placeholders (eg. "etw.")
+        # and translation_items
+        if not any(
+            cls in html.attrs["class"]
+            for cls in ["main_item", "translation_item", "placeholder"]
+        ):
+            return ""
+
+        return " ".join([str_from_html(tr) for tr in html.children]).strip()
 
 
-def getLinguee(query):
+def get_suggestions(query):
     response = requests.get(
-        "http://www.linguee.de/" + lang + "/search?",
-        params={"qe": query, "source": "auto"},
-    ).text
-    response = response.replace("<span class='sep'>&middot;</span>", "")
-    response = response.replace("&", "#-#")
+        "https://www.linguee.de/" + lang + "/search?",
+        params={"qe": query, "source": "auto", "cw": "820", "ch": "544"},
+    )
+    parsed = bs4.BeautifulSoup(response.text, "html.parser")
+    # get all suggestions
+    items = parsed.find_all("div", attrs={"class": "autocompletion_item"})
+    results = []
+    for item in items:
+        # main_item is the suggested word
+        result = {
+            "word": str_from_html(item.find("div", attrs={"class": "main_item"})),
+            "translations": [],
+        }
+        # find all translation_items for this item
+        for transl in item.find_all("div", attrs={"class": "translation_item"}):
+            transl_str = str_from_html(transl)
+            result["translations"].append(transl_str)
+        results.append(result)
 
-    root = ET.fromstring(response)
-    result = []
-    for suggestion in root:
-        word = html.unescape(suggestion[0][0].text.strip().replace("#-##", "&#"))
-        trans = []
-        if len(suggestion) > 1 and len(suggestion[1]) > 0:
-            for translation in suggestion[1][
-                0
-            ]:  # note that this yields only the first line of results
-                current = ""
-                for text in translation.itertext():
-                    text = html.unescape(text.strip().replace("#-##", "&#"))
-                    if isbadText(text):
-                        continue
-                    current += text + " "
-                trans += [(current.strip())]
-        result += [{"word": word, "trans": trans}]
-    return result
+    return results
 
 
 def handleQuery(query):
     results = []
     if query.isTriggered:
-        if len(query.string) > 2:
-
-            for res in getLinguee(query.string):
-                results.append(
-                    Item(
-                        id=res["word"],
-                        icon=iconPath,
-                        text=res["word"],
-                        subtext=", ".join(res["trans"]),
-                        completion=__trigger__ + res["word"],
-                        actions=[
-                            UrlAction(
-                                "Open",
-                                "http://www.linguee.de/"
-                                + lang
-                                + "/search?source=auto&query="
-                                + res["word"],
-                            )
-                        ],
-                    )
+        for res in get_suggestions(query.string):
+            results.append(
+                Item(
+                    id=res["word"],
+                    icon=iconPath,
+                    text=res["word"],
+                    subtext=", ".join(res["translations"]),
+                    completion=__trigger__ + res["word"],
+                    actions=[
+                        UrlAction(
+                            "Open",
+                            "http://www.linguee.de/"
+                            + lang
+                            + "/search?source=auto&query="
+                            + res["word"],
+                        )
+                    ],
                 )
-            if len(results) == 0:
-                return [getItem("Sorry, there are no results.")]
-        else:
-            return [getItem("Type at least 3 chars.")]
+            )
+        if len(results) == 0:
+            return [getItem("Sorry, there are no results.")]
 
     return results
