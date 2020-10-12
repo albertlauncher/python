fb067b388b4dda78c787f52c94341c4daab834da
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2020-10-12 11:31:44 +0200

    Add bitwarden extension

---
+++ bitwarden.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+"""'rbw' wrapper extension."""
+
+import fnmatch
+import os
+from shutil import which
+from subprocess import run
+
+from albertv0 import *
+
+__iid__ = "PythonInterface/v0.1"
+__prettyname__ = "Bitwarden"
+__version__ = "1.0"
+__trigger__ = "bw "
+__author__ = "Asger Hautop Drewsen"
+__dependencies__ = ["rbw"]
+
+
+if which("rbw") is None:
+    raise Exception("'rbw' is not in $PATH.")
+
+ICON_PATH = iconLookup("dialog-password")
+
+
+def handleQuery(query):
+    if not query.isTriggered:
+        return
+
+    passwords = get_passwords()
+    filtered_passwords = []
+    search_fields = ["name", "user", "folder"]
+    words = query.string.strip().lower().split()
+    for p in passwords:
+        all_matches = True
+        for w in words:
+            for k in search_fields:
+                if w in p[k].lower():
+                    break
+            else:
+                all_matches = False
+                break
+
+        if all_matches:
+            filtered_passwords.append(p)
+
+    results = []
+    for p in filtered_passwords:
+        results.append(
+            Item(
+                id=p["id"],
+                icon=ICON_PATH,
+                text=p["name"],
+                subtext=p["user"],
+                completion=f"rbw {p['name']}",
+                actions=[
+                    ProcAction(
+                        "Copy",
+                        [
+                            "sh",
+                            "-c",
+                            f"rbw get {p['id']} | tr -d '\\n' | xclip -selection clipboard -in",
+                        ],
+                    ),
+                    TermAction("Edit", ["rbw", "edit", p["id"]]),
+                ],
+            ),
+        )
+
+    return results
+
+
+def get_passwords():
+    field_names = ["id", "name", "user", "folder"]
+    p = run(
+        ["rbw", "list", "--fields", *field_names],
+        capture_output=True,
+        encoding="utf-8",
+        check=True,
+    )
+    passwords = []
+    for l in p.stdout.splitlines():
+        fields = l.split("\t")
+        d = {}
+        passwords.append(dict(zip(field_names, fields)))
+
+    return passwords
