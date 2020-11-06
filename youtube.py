7951b02c08551a4f1fbc48fec2b4734dafc4ec18
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-11-06 20:27:29 +0000

    Bail out if we can't find the initial data object
    Instead of checking for a match and just doing nothing if it fails, this
    throws a critical log stating what happened and saves the HTML received
    from youtube.com/results to a file in /tmp for debugging.

--- youtube.py
+++ youtube.py
@@ -11,7 +11,7 @@ from os import path
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
-from albertv0 import Item, UrlAction, iconLookup, critical
+from albertv0 import Item, UrlAction, iconLookup, critical, info
 
 __iid__ = 'PythonInterface/v0.1'
 __prettyname__ = 'Youtube'
@@ -37,57 +37,76 @@ def handleQuery(query):
         if not query.isValid:
             return
 
+        info("Searching YouTube for '{}'".format(query.string))
         req = Request(headers=HEADERS, url='https://www.youtube.com/results?{}'.format(
             urlencode({ 'search_query': query.string.strip() })))
 
         with urlopen(req) as response:
-            match = re.search(re_videos, response.read().decode())
-            if match:
-                results = json.loads(match.group(1))
-                results = results['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents']
-                items = []
-                for result in results:
-                    for type, data in result.items():
-                        try:
-                            if type == 'videoRenderer':
-                                subtext = ['Video']
-                                action = 'Watch on Youtube'
-                                link = 'watch?v={}'.format(data['videoId'])
+            responseBytes = response.read()
+            match = re.search(re_videos, responseBytes.decode())
+            if match is None:
+                critical("Failed to receive expected data from YouTube. This likely means API changes, but could just be a failed request.")
+                logHtml(responseBytes)
+                return
 
-                                if 'lengthText' in data:
-                                    subtext.append(textFrom(data['lengthText']))
-                                if 'shortViewCountText' in data:
-                                    subtext.append(textFrom(data['shortViewCountText']))
-                                if 'publishedTimeText' in data:
-                                    subtext.append(textFrom(data['publishedTimeText']))
+            results = json.loads(match.group(1))
+            results = results['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents']
+            items = []
+            for result in results:
+                for type, data in result.items():
+                    try:
+                        if type == 'videoRenderer':
+                            subtext = ['Video']
+                            action = 'Watch on Youtube'
+                            link = 'watch?v={}'.format(data['videoId'])
 
-                            elif type == 'channelRenderer':
-                                subtext = ['Channel']
-                                action = 'Show on Youtube'
-                                link = 'channel/{}'.format(data['channelId'])
+                            if 'lengthText' in data:
+                                subtext.append(textFrom(data['lengthText']))
+                            if 'shortViewCountText' in data:
+                                subtext.append(textFrom(data['shortViewCountText']))
+                            if 'publishedTimeText' in data:
+                                subtext.append(textFrom(data['publishedTimeText']))
 
-                                if 'videoCountText' in data:
-                                    subtext.append(textFrom(data['videoCountText']))
-                                if 'subscriberCountText' in data:
-                                    subtext.append(textFrom(data['subscriberCountText']))
-                            else:
-                                continue
-                        except Exception as e:
-                            critical(e)
-                            critical(json.dumps(result, indent=4))
+                        elif type == 'channelRenderer':
+                            subtext = ['Channel']
+                            action = 'Show on Youtube'
+                            link = 'channel/{}'.format(data['channelId'])
 
-                        item = Item(id=__prettyname__,
-                                    icon=data['thumbnail']['thumbnails'][0]['url'].split('?', 1)[0] if data['thumbnail']['thumbnails'] else __icon__,
-                                    text=textFrom(data['title']),
-                                    subtext=' | '.join(subtext),
-                                    completion=query.rawString,
-                                    actions=[ UrlAction(action, 'https://www.youtube.com/' + link) ]
-                                )
-                        items.append(item)
-                return items
+                            if 'videoCountText' in data:
+                                subtext.append(textFrom(data['videoCountText']))
+                            if 'subscriberCountText' in data:
+                                subtext.append(textFrom(data['subscriberCountText']))
+                        else:
+                            continue
+                    except Exception as e:
+                        critical(e)
+                        critical(json.dumps(result, indent=4))
+
+                    item = Item(id=__prettyname__,
+                                icon=data['thumbnail']['thumbnails'][0]['url'].split('?', 1)[0] if data['thumbnail']['thumbnails'] else __icon__,
+                                text=textFrom(data['title']),
+                                subtext=' | '.join(subtext),
+                                completion=query.rawString,
+                                actions=[ UrlAction(action, 'https://www.youtube.com/' + link) ]
+                            )
+                    items.append(item)
+            return items
 
 def textFrom(val):
     text = val['simpleText'] if 'runs' not in val else \
         ''.join('{}'.format(v['text']) for v in val['runs'])
 
     return text.strip()
+
+def logHtml(html):
+    logTime = time.strftime("%Y%m%d-%H%M%S")
+    logName = 'albert.plugins.youtube_dump'
+    logPath = '/tmp/{}-{}.html'.format(logName, logTime)
+
+    f = open(logPath, 'wb')
+    f.write(html)
+    f.close()
+
+    critical("The HTML output has been dumped to {}.".format(logPath))
+    critical("If the page looks ok in a browser, please include the dump in a new issue:")
+    critical("  https://www.github.com/albertlauncher/albert/issues/new")
