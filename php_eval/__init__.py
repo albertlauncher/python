c0f669b0d40e8fffaa2c938c391e3e2ef1e2b463
Hammed Oyedele <itz.harmid@gmail.com>
2020-11-09 11:05:29 +0100

    [php] Add new extension: php evaluator

---
+++ php_eval/__init__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""Evaluate simple PHP expressions. Use it with care every keystroke triggers an evaluation."""
+
+import os
+import subprocess
+from albertv0 import *
+from shutil import which
+
+__iid__ = 'PythonInterface/v0.1'
+__prettyname__ = 'PHP Eval'
+__version__ = '1.0'
+__trigger__ = 'php '
+__author__ = 'Hammed Oyedele'
+__dependencies__ = ['php']
+
+iconPath = os.path.dirname(__file__) + '/php.svg'
+
+if which('php') is None:
+    raise Exception('"php" is not in $PATH.')
+
+
+def run(exp):
+    return subprocess.getoutput('php -r "%s"' % exp.replace('"', '\\"'))
+
+
+def handleQuery(query):
+    if query.isTriggered:
+        item = Item(
+            id=__prettyname__,
+            icon=iconPath,
+            completion=query.rawString,
+        )
+        stripped = query.string.strip()
+
+        if stripped == '':
+            item.text = 'Enter a PHP expression...'
+        else:
+            item.text = run('echo %s;' % stripped)
+            item.subtext = run('echo gettype(%s);' % stripped)
+            item.addAction(ClipAction('Copy result to clipboard', item.text))
+
+        return item
