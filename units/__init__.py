1791991fe6eb63ddd70504f780c51b2cfe3b06be
iyzana <16743652+iyzana@users.noreply.github.com>
2020-12-08 22:46:26 +0100

    [units] v1.2 including to time conversion

--- units/__init__.py
+++ units/__init__.py
@@ -17,9 +17,9 @@ from albertv0 import *
 
 __iid__ = "PythonInterface/v0.1"
 __prettyname__ = "GNU Units"
-__version__ = "1.1"
+__version__ = "1.2"
 __trigger__ = "units "
-__author__ = "Manuel Schneider"
+__author__ = "Manuel Schneider, iyzana"
 __dependencies__ = ["units"]
 
 if which("units") is None:
@@ -30,33 +30,54 @@ if not icon:
     icon = ":python_module"
 
 regex = re.compile(r"(\S+)(?:\s+to)\s+(\S+)")
+unitListOutput = re.compile(r"(\d+(e[+-]\d{2,})?;)+[\d.]+(e[+-]\d{2,})?")
+
+
+def getUnitsResult(args):
+    command = ['units', '--terse', '--'] + list(args)
+    query = "units -t -- %s" % ' '.join(args)
+    try:
+        output = sp.check_output(command, stderr=sp.STDOUT).decode().strip()
+
+        # usually we want terse output, but when we get a unit-list output
+        # it looks like this 1;124;18;11;14.025322 which is not friendly
+        # so we're falling back to not quite terse output
+        if unitListOutput.fullmatch(output):
+            command = ['units', '--strict', '--one-line',
+                       '--quiet', '--'] + list(args)
+            query = "units -s1q -- %s" % ' '.join(args)
+            output = sp.check_output(
+                command, stderr=sp.STDOUT).decode().strip()
+
+        return (output, query, True)
+    except sp.CalledProcessError as e:
+        return (e.stdout.decode().strip().splitlines()[0], query, False)
+
 
 def handleQuery(query):
-
     if query.isTriggered:
         args = query.string.split()
-        item = Item(id='python.gnu_units', icon=icon, completion=query.rawString)
+        item = Item(id='python.gnu_units', icon=icon,
+                    completion=query.rawString)
         if args:
-            try:
-                item.text = sp.check_output(['units', '-t'] + query.string.split(), stderr=sp.STDOUT).decode().strip()
-                item.addAction(ClipAction("Copy to clipboard", item.text))
-            except sp.CalledProcessError as e:
-                item.text = e.stdout.decode().strip().partition('\n')[0]
-            item.subtext = "Result of 'units -t %s'" % query.string
+            result, command, success = getUnitsResult(args)
+            item.text = result
+            item.subtext = "Result of '%s'" % command
+            item.addAction(ClipAction("Copy to clipboard", item.text))
         else:
             item.text = "Empty input"
-            item.subtext = "Enter something to convert"
+            item.subtext = "Enter a query of the form <from> [<to>]"
         return item
-
     else:
         match = regex.fullmatch(query.string.strip())
         if match:
             args = match.group(1, 2)
-            try:
-                item = Item(id='python.gnu_units', icon=icon, completion=query.rawString)
-                item.text = sp.check_output(['units', '-t'] + list(args)).decode().strip()
-                item.subtext = "Result of 'units -t %s %s'" % args
-                item.addAction(ClipAction("Copy to clipboard", item.text))
-                return item
-            except sp.CalledProcessError as e:
-                pass
+            result, command, success = getUnitsResult(args)
+            if not success:
+                return
+            item = Item(id='python.gnu_units', icon=icon,
+                        completion=query.rawString)
+            item.text = result
+            item.subtext = "Result of '%s'" % command
+            item.addAction(ClipAction("Copy to clipboard", item.text))
+            return item
