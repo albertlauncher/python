96a0bb03c7c0299ad7f67f2cadfd948e2c6c291a
Manuel Schneider <manuelschneid3r@gmail.com>
2022-12-27 12:51:34 +0100

    trash 1.2 port 0.18

---
+++ trash/__init__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+"""
+This extension provides a single item which opens the
+systems trash location in your default file manager.
+"""
+
+from albert import *
+import os
+from platform import system
+from pathlib import Path
+
+md_iid = "0.5"
+md_version = "1.2"
+md_name = "Trash"
+md_description = "Open trash"
+md_license = "BSD-3"
+md_url = "https://github.com/albertlauncher/python/tree/master/trash"
+md_authors = md_maintainers = "@manuelschneid3r"
+
+class Plugin(QueryHandler):
+    def id(self):
+        return __name__
+
+    def name(self):
+        return md_name
+
+    def description(self):
+        return md_description
+
+    def initialize(self):
+        if (system() == 'Linux'):
+            trash_path  = "trash:///"
+            icon = ["xdg:user-trash-full", os.path.dirname(__file__)+"/trash.svg"]
+        elif (system() == 'Darwin'):
+            trash_path  = "file://%s/.Trash" % Path.home()
+            icon = [os.path.dirname(__file__)+"/trash.svg"]
+        else:
+            raise NotImplementedError("%1 not supported" % system())
+
+        self.trash_item = Item(id="trash-open",
+                               text="Trash",
+                               subtext="Open trash folder",
+                               icon=icon,
+                               actions=[Action("trash-open", "Open trash", lambda path=trash_path: openUrl(path))])
+
+    def handleQuery(self, query):
+        query.add(self.trash_item)
