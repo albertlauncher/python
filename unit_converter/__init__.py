9511c21106b18152ce48b38be509108a16144aab
Manuel Schneider <manuelschneid3r@gmail.com>
2023-01-25 19:57:34 +0100

    [pint] minor adjustments

--- unit_converter/__init__.py
+++ unit_converter/__init__.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
 
+"""
+Unit converter based on the [Pint Python library](https://pint.readthedocs.io/en/stable/).
+
+Usage examples:
+- `convert 180 minutes to hrs`
+- `convert 100 km to miles`
+- `convert 88 mph to kph`
+- `convert 32 degrees F to C`
+- `convert 3.14159 rad to degrees`
+- `convert 100 USD to EUR`
+"""
+
+
 from __future__ import annotations
 
 import json
@@ -15,22 +28,6 @@ import albert
 import inflect
 import pint
 
-__doc__ = """
-Extension for converting units of length, mass, speed, temperature, time,
-current, luminosity, printing measurements, molecular substance, and currency.
-
-All units from the Pint Python library are supported.
-
-Currency conversion rates are provided by https://www.exchangerate-api.com
-
-Examples:
-`convert 180 minutes to hrs`
-`convert 100 km to miles`
-`convert 88 mph to kph`
-`convert 32 degrees F to C`
-`convert 3.14159 rad to degrees`
-`convert 100 USD to EUR`
-"""
 
 md_iid = "0.5"
 md_version = "1.2"
