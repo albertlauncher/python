6893452658d422a9877c2a3b19296d49d033443a
Markus Richter <8398165+mqus@users.noreply.github.com>
2023-01-27 22:14:48 +0100

    update to current state in github.com/mqus/jetbrains/albert-plugin
    current HEAD there: db16fc87898257215d2cc8eba5e7f61ecb921a56

--- jetbrains_projects/README.md
+++ jetbrains_projects/README.md
@@ -1,39 +1,24 @@
 # Jetbrains-albert-plugin
 
-This is a plugin for the albert launcher(https://albertlauncher.github.io/) which lists and lets you start projects of Jetbrains IDEs such as IntelliJ IDEA, PyCharm, GoLand, etc.
+DISCLAIMER: This plugin has no affiliation with JetBrains s.r.o. The included fallback logo `jetbrains.svg` is not my property, I am using it under the terms specified [here](https://www.jetbrains.com/company/useterms.html).
 
-I made this project to maintain this plugin, which was originally part of the albert community plugins. The plugin itself(the python file(s)) is licensed under the GPLv3 license.
+The plugin itself(the python file(s)) is licensed under the GPLv3 license.
 
-DISCLAIMER: I have no affiliation with JetBrains s.r.o. The included fallback logo `jetbrains.svg` is not my property, I am using it under the terms specified [here](https://www.jetbrains.com/company/useterms.html).
+## How to use:
 
-## How to install:
-If you are using git, you can use the following command which will clone the repository into the right directory (respecting the XDG standard).
-```
-$ git clone https://github.com/mqus/jetbrains-albert-plugin.git ${XDG_DATA_HOME:-$HOME/.local/share}/albert/org.albert.extension.python/modules/jetbrains-projects
-```
+Type `jb ` in the prompt, followed by your search term. Albert should show you a list of matching projects, scraped from your "recently edited" list of your Jetbrains IDEs. It tries to match the path of your project directory (not e.g. any files or contents).
 
+## Creating the project launcher:
 
-If you don't have git, you can simply download a zip archive, extract it and move the folder containing `__init__.py` into the `.local/share/albert/org.albert.extension.python/modules` directory in your home directory, while creating any folders that don't exist yet.
+For this plugin to find the editors, you need to create a command line launcher for them. This is done by opening the IDE (for example PyCharm) and then going to `Tools -> Create Command-line Launcher...`. This will create a launcher (for example, `charm`) that this plugin will be able to find.
 
-After that, you should have the following structure:
-```
-[...]
-'- modules
-   '- <the plugin directory>
-      |- __init.py
-      '- jetbrains.svg
+## Authors:
 
-```
-
-Now, open your albert settings and go to Extensions and then Python. You can then activate the plugin there. You might have to restart albert if it does not show up there at first.
+- Thomas Queste (@tomsquest)
+- @mqus
 
 ## Contributors:
 
-Thomas Queste (@tomsquest)
-
-@iyzana
-
-@Sharsie
-
-@dsager
-
+- @iyzana
+- @Sharsie
+- @dsager
--- jetbrains_projects/__init__.py
+++ jetbrains_projects/__init__.py
@@ -2,32 +2,40 @@
 
 """List and open JetBrains IDE projects."""
 
-#  Copyright (c) 2022 Manuel Schneider
-
 import os
+import time
 from shutil import which
 from xml.etree import ElementTree
 
-from albertv0 import *
+from albert import *
 
-__iid__ = "PythonInterface/v0.1"
-__prettyname__ = "Jetbrains IDE Projects"
-__version__ = "1.3"
-__trigger__ = "jb "
-__author__ = "Markus Richter, Thomas Queste"
-__dependencies__ = []
+md_iid = "0.5"
+md_version = "0.5"
+md_name = "Jetbrains"
+md_description = "Open Jetbrains IDE projects"
+md_license = "GPL-3"
+md_url = "https://github.com/mqus/jetbrains-albert-plugin"
+md_maintainers = ["@mqus", "@tomsquest"]
+md_authors = "@mqus"
 
 default_icon = os.path.dirname(__file__) + "/jetbrains.svg"
 HOME_DIR = os.environ["HOME"]
-JETBRAINS_XDG_CONFIG_DIR = os.path.join(HOME_DIR, ".config/JetBrains")
 
-paths = [  # <Name for config directory>, <possible names for the binary/icon>
+JETBRAINS_XDG_CONFIG_DIR = os.path.join(HOME_DIR, ".config/JetBrains")
+GOOGLE_XDG_CONFIG_DIR = os.path.join(HOME_DIR, ".config/Google")
+
+# for all new (2020.1+) config folders and IntelliJIdea and AndroidStudio, this is the right path.
+NEW_RELATIVE_CONFIG_PATH = "options/recentProjects.xml"
+# for older config folders of other IDEs, the following is right:
+OLD_RELATIVE_CONFIG_PATH = "options/recentProjectDirectories.xml"
+
+# <Name for config directory>, <possible names for the binary/icon>
+paths = [
     ["AndroidStudio", "android-studio"],
     ["CLion", "clion"],
     ["DataGrip", "datagrip"],
     ["GoLand", "goland"],
-    ["IntelliJIdea",
-     "intellij-idea-ue-bundled-jre intellij-idea-ultimate-edition idea-ce-eap idea-ue-eap idea idea-ultimate"],
+    ["IntelliJIdea", "intellij-idea-ue-bundled-jre intellij-idea-ultimate-edition idea-ce-eap idea-ue-eap idea idea-ultimate"],
     ["PhpStorm", "phpstorm"],
     ["PyCharm", "pycharm pycharm-eap charm"],
     ["RubyMine", "rubymine jetbrains-rubymine jetbrains-rubymine-eap"],
@@ -35,110 +43,178 @@ paths = [  # <Name for config directory>, <possible names for the binary/icon>
 ]
 
 
-# find the executable path and icon of a program described by space-separated lists of possible binary-names
-def find_exec(namestr: str):
-    for name in namestr.split(" "):
-        executable = which(name)
-        if executable:
-            icon = iconLookup(name) or default_icon
-            return executable, icon
-    return None
+class Plugin(QueryHandler):
+    def id(self):
+        return __name__
 
+    def name(self):
+        return md_name
 
-# parse the xml at path, return all recent project paths and the time they were last open
-def get_proj(path):
-    r = ElementTree.parse(path).getroot()  # type:ElementTree.Element
-    add_info = None
-    items = dict()
-    for o in r[0]:  # type:ElementTree.Element
-        if o.attrib["name"] == 'recentPaths':
-            for i in o[0]:
-                items[i.attrib["value"]] = 0
+    def description(self):
+        return md_description
 
-        else:
-            if o.attrib["name"] == 'additionalInfo':
-                add_info = o[0]
+    def defaultTrigger(self):
+        return "jb "
 
-    if len(items) == 0:
-        return []
+    def initialize(self):
+        pass
 
-    if add_info is not None:
-        for i in add_info:
-            for o in i[0][0]:
-                if o.tag == 'option' and 'name' in o.attrib and o.attrib["name"] == 'projectOpenTimestamp':
-                    items[i.attrib["key"]] = int(o.attrib["value"])
-    return [(items[e], e.replace("$USER_HOME$", HOME_DIR)) for e in items]
-
-
-def handleQuery(query):
-    if query.isTriggered:
+    def handleQuery(self, query: Query):
+        # a dict which maps the app name to a tuple of executable path and icon.
         binaries = {}
+        # an array of tuples representing the project([timestamp,path,app name])
         projects = []
 
         for app in paths:
-            config_path = "options/recentProjects.xml"
+            # get configuration file path
+            full_config_path = self.find_config_path(app[0])
 
-            full_config_path = None
-
-            # newer versions (since 2020.1) put their configuration here
-            if os.path.isdir(JETBRAINS_XDG_CONFIG_DIR):
-                # dirs contains possibly multiple directories for a program (eg. .GoLand2018.1 and .GoLand2017.3)
-                dirs = [f for f in os.listdir(JETBRAINS_XDG_CONFIG_DIR) if
-                        os.path.isdir(os.path.join(JETBRAINS_XDG_CONFIG_DIR, f)) and f.startswith(app[0])]
-                # take the newest
-                dirs.sort(reverse=True)
-                if len(dirs) != 0:
-                    full_config_path = os.path.join(JETBRAINS_XDG_CONFIG_DIR, dirs[0], config_path)
-
-            # if no config was found in the newer path, repeat for the old ones
-            if full_config_path is None or not os.path.exists(full_config_path):
-                if app[0] != "IntelliJIdea" and app[0] != "AndroidStudio":
-                    config_path = "options/recentProjectDirectories.xml"
-                # dirs contains possibly multiple directories for a program (eg. .GoLand2018.1 and .GoLand2017.3)
-                dirs = [f for f in os.listdir(HOME_DIR) if
-                        os.path.isdir(os.path.join(HOME_DIR, f)) and f.startswith("." + app[0])]
-                # take the newest
-                dirs.sort(reverse=True)
-                if len(dirs) == 0:
-                    continue
-
-                full_config_path = os.path.join(HOME_DIR, dirs[0], "config", config_path)
-                if not os.path.exists(full_config_path):
-                    continue
+            if full_config_path is None:
+                continue
 
             # extract the binary name and icon
-            binaries[app[0]] = find_exec(app[1])
+            binaries[app[0]] = self.find_exec(app[1])
 
             # add all recently opened projects
-            projects.extend([[e[0], e[1], app[0]] for e in get_proj(full_config_path)])
-        projects.sort(key=lambda s: s[0], reverse=True)
+            projects.extend(
+                [[e[0], e[1], app[0]] for e in self.get_proj(full_config_path)]
+            )
 
         # List all projects or the one corresponding to the query
         if query.string:
-            projects = [p for p in projects if p[1].lower().find(query.string.lower()) != -1]
+            projects = [
+                p for p in projects if p[1].lower().find(query.string.lower()) != -1
+            ]
+
+        # sort by last modified, most recent first.
+        projects.sort(key=lambda p: p[0], reverse=True)
 
         items = []
+        now = int(time.time() * 1000.0)
         for p in projects:
             last_update = p[0]
             project_path = p[1]
+            if not os.path.exists(project_path):
+                continue
             project_dir = project_path.split("/")[-1]
-            product_name = p[2]
-            binary = binaries[product_name]
+            app_name = p[2]
+            binary = binaries[app_name]
             if not binary:
                 continue
 
-            executable = binaries[p[2]][0]
-            icon = binaries[p[2]][1]
+            executable = binary[0]
+            icon = binary[1]
 
-            items.append(Item(
-                id="-" + str(last_update),
-                icon=icon,
-                text=project_dir,
-                subtext=project_path,
-                completion=__trigger__ + project_dir,
-                actions=[
-                    ProcAction("Open in %s" % product_name, [executable, project_path])
-                ]
-            ))
+            items.append(
+                Item(
+                    id="%015d-%s-%s" % (now - last_update, project_path, app_name),
+                    text=project_dir,
+                    subtext="Open %s with %s" % (project_path, app_name),
+                    completion=query.trigger + project_dir,
+                    icon=[icon, default_icon],
+                    actions=[
+                        Action(
+                            "Open",
+                            "Open in %s" % app_name,
+                            lambda selected_project=project_path: runDetachedProcess(
+                                [executable, selected_project]
+                            ),
+                        )
+                    ],
+                )
+            )
 
-        return items
+        query.add(items)
+
+    # find the executable path and icon of a program described by space-separated lists of possible binary-names
+    def find_exec(self, namestr: str):
+        for name in namestr.split(" "):
+            executable = which(name)
+            if executable:
+                return executable, "xdg:%s" % name
+        else:
+            return None
+
+    # parse the xml at path, return all recent project paths and the time they were last open
+    def get_proj(self, path: str):
+        root = ElementTree.parse(path).getroot()  # type:ElementTree.Element
+        add_info = None
+        path2timestamp = dict()
+        for option_tag in root[0]:  # type:ElementTree.Element
+            if option_tag.attrib["name"] == "recentPaths":
+                for recent_path in option_tag[0]:
+                    path2timestamp[recent_path.attrib["value"]] = 0
+            elif option_tag.attrib["name"] == "additionalInfo":
+                add_info = option_tag[0]
+
+        # for all additionalInfo entries, also add the real timestamp.
+        if add_info is not None:
+            for entry_tag in add_info:
+                for option_tag in entry_tag[0][0]:
+                    if (
+                        option_tag.tag == "option"
+                        and "name" in option_tag.attrib
+                        and option_tag.attrib["name"] == "projectOpenTimestamp"
+                    ):
+                        path2timestamp[entry_tag.attrib["key"]] = int(
+                            option_tag.attrib["value"]
+                        )
+
+        # return [(timestamp,path),...]
+        return [
+            (path2timestamp[path], path.replace("$USER_HOME$", HOME_DIR))
+            for path in path2timestamp
+        ]
+
+    # finds the actual path to the relevant xml file of the most recent configuration directory
+    def find_config_path(self, app_name: str):
+        full_config_path = None
+
+        xdg_dir = (
+            GOOGLE_XDG_CONFIG_DIR
+            if app_name == "AndroidStudio"
+            else JETBRAINS_XDG_CONFIG_DIR
+        )
+
+        # newer versions (since 2020.1) put their configuration here
+        if os.path.isdir(xdg_dir):
+            # dirs contains possibly multiple directories for a program (eg. .GoLand2018.1 and .GoLand2017.3)
+            dirs = [
+                f
+                for f in os.listdir(xdg_dir)
+                if os.path.isdir(os.path.join(xdg_dir, f)) and f.startswith(app_name)
+            ]
+            # take the newest
+            dirs.sort(reverse=True)
+            if len(dirs) != 0:
+                full_config_path = os.path.join(
+                    xdg_dir, dirs[0], NEW_RELATIVE_CONFIG_PATH
+                )
+
+        # if no config was found in the newer path, repeat for the old ones
+        if full_config_path is None or not os.path.exists(full_config_path):
+
+            # dirs contains possibly multiple directories for a program (eg. .GoLand2018.1 and .GoLand2017.3)
+            dirs = [
+                f
+                for f in os.listdir(HOME_DIR)
+                if os.path.isdir(os.path.join(HOME_DIR, f))
+                and f.startswith("." + app_name)
+            ]
+            # take the newest
+            dirs.sort(reverse=True)
+            if len(dirs) == 0:
+                return None
+
+            if app_name != "IntelliJIdea" and app_name != "AndroidStudio":
+                full_config_path = os.path.join(
+                    HOME_DIR, dirs[0], "config", OLD_RELATIVE_CONFIG_PATH
+                )
+            else:
+                full_config_path = os.path.join(
+                    HOME_DIR, dirs[0], "config", NEW_RELATIVE_CONFIG_PATH
+                )
+
+            if not os.path.exists(full_config_path):
+                return None
+        return full_config_path
