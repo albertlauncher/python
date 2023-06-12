800fc53366a1e088c706107b507104f70c65ac31
Thomas Queste <tom@tomsquest.com>
2023-06-12 11:26:09 +0200

    feat(jetbrains_project): add `idea.sh` to the list of possible binaries
    The latest version of Intellij deprecated the `idea` script.
    They suggested adding `idea.sh` to $PATH.
    This changes adds the `idea.sh` to the list of binaries that are searched for when the plugin starts.

--- jetbrains_projects/__init__.py
+++ jetbrains_projects/__init__.py
@@ -130,7 +130,7 @@ class Plugin(TriggerQueryHandler):
                 name="IntelliJ IDEA",
                 icon=plugin_dir / "idea.svg",
                 config_dir_prefix="JetBrains/IntelliJIdea",
-                binaries=["idea", "idea-ultimate", "idea-ce-eap", "idea-ue-eap", "intellij-idea-ce",
+                binaries=["idea", "idea.sh", "idea-ultimate", "idea-ce-eap", "idea-ue-eap", "intellij-idea-ce",
                           "intellij-idea-ce-eap", "intellij-idea-ue-bundled-jre", "intellij-idea-ultimate-edition",
                           "intellij-idea-community-edition-jre", "intellij-idea-community-edition-no-jre"]),
             Editor(
