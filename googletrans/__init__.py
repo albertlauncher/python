e11d5c47af7860b76c21c70f87a23bcfc93a7004
Manuel Schneider <manuelschneid3r@gmail.com>
2023-08-03 01:57:00 +0200

    [googletrans] Interface v2.0

--- googletrans/__init__.py
+++ googletrans/__init__.py
@@ -4,14 +4,15 @@
 Translator using py-googletrans
 """
 
+from locale import getdefaultlocale
+from pathlib import Path
+from time import sleep
+
 from albert import *
 from googletrans import Translator, LANGUAGES
-from locale import getdefaultlocale
-from time import sleep
-import os
 
-md_iid = '1.0'
-md_version = "1.1"
+md_iid = '2.0'
+md_version = "1.2"
 md_name = "Google Translate"
 md_description = "Translate sentences using googletrans"
 md_license = "BSD-3"
@@ -19,32 +20,25 @@ md_url = "https://github.com/albertlauncher/python/"
 md_lib_dependencies = "googletrans==3.1.0a0"
 md_maintainers = "@manuelschneid3r"
 
+
 class Plugin(TriggerQueryHandler):
 
-    def id(self):
-        return md_id
-
-    def name(self):
-        return md_name
-
-    def description(self):
-        return md_description
-
-    def defaultTrigger(self):
-        return "tr "
-
-    def synopsis(self):
-        return "[[src] dest] text"
-
-    def initialize(self):
-        self.icon = [os.path.dirname(__file__)+"/google_translate.png"]
+    def __init__(self):
+        TriggerQueryHandler.__init__(self,
+                                     id=md_id,
+                                     name=md_name,
+                                     description=md_description,
+                                     synopsis="[[src] dest] text",
+                                     defaultTrigger='tr ')
+        PluginInstance.__init__(self, extensions=[self])
+        self.iconUrls = [f"file:{Path(__file__).parent}/google_translate.png"]
         self.translator = Translator()
         self.lang = getdefaultlocale()[0][0:2]
 
     def handleTriggerQuery(self, query):
         stripped = query.string.strip()
         if stripped:
-            for number in range(50):
+            for _ in range(50):
                 sleep(0.01)
                 if not query.isValid:
                     return
@@ -64,11 +58,11 @@ class Plugin(TriggerQueryHandler):
             else:
                 translation = self.translator.translate(text, dest=dest)
 
-            query.add(Item(
+            query.add(StandardItem(
                 id=md_id,
                 text=translation.text,
                 subtext=f'From {LANGUAGES[translation.src]} to {LANGUAGES[translation.dest]}',
-                icon=self.icon,
-                actions = [Action("copy", "Copy result to clipboard",
-                                  lambda t=translation.text: setClipboardText(t))]
+                iconUrls=self.iconUrls,
+                actions=[Action("copy", "Copy result to clipboard",
+                                lambda t=translation.text: setClipboardText(t))]
             ))
