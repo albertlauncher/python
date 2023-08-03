c409e3feab2708740f03c0ba6dd6abbf4a691b74
Manuel Schneider <manuelschneid3r@gmail.com>
2023-08-03 02:05:07 +0200

    [pass] Interface v2.0

--- pass/__init__.py
+++ pass/__init__.py
@@ -4,8 +4,8 @@ import fnmatch
 import os
 from albert import *
 
-md_iid = '1.0'
-md_version = "1.3"
+md_iid = '2.0'
+md_version = "1.4"
 md_name = "Pass"
 md_description = "Manage passwords in pass"
 md_bin_dependencies = ["pass"]
@@ -14,24 +14,19 @@ md_license = "BSD-3"
 
 HOME_DIR = os.environ["HOME"]
 PASS_DIR = os.environ.get("PASSWORD_STORE_DIR", os.path.join(HOME_DIR, ".password-store/"))
-ICON = ["xdg:dialog-password"]
 
-class Plugin(TriggerQueryHandler):
 
-    def id(self):
-        return md_id
+class Plugin(PluginInstance, TriggerQueryHandler):
 
-    def name(self):
-        return md_name
-
-    def description(self):
-        return md_description
-
-    def synopsis(self):
-        return "<pass-name>"
-
-    def defaultTrigger(self):
-        return "pass "
+    def __init__(self):
+        TriggerQueryHandler.__init__(self,
+                                     id=md_id,
+                                     name=md_name,
+                                     description=md_description,
+                                     synopsis='<pass-name>',
+                                     defaultTrigger='pass ')
+        PluginInstance.__init__(self, extensions=[self])
+        self.iconUrls = ["xdg:dialog-password"]
 
     def handleTriggerQuery(self, query):
         if query.string.strip().startswith("generate"):
@@ -42,19 +37,18 @@ class Plugin(TriggerQueryHandler):
     def generatePassword(self, query):
         location = query.string.strip()[9:]
 
-        query.add(Item(
+        query.add(StandardItem(
             id="generate_password",
-            icon=ICON,
+            iconUrls=self.iconUrls,
             text="Generate a new password",
             subtext="The new password will be located at %s" % location,
-            completion="pass %s" % query.string,
+            inputActionText="pass %s" % query.string,
             actions=[
                 Action("generate", "Generate", lambda: runDetachedProcess(["pass", "generate", "--clip", location, "20"]))
             ]
         ))
 
     def showPasswords(self, query):
-        passwords = []
         if query.string.strip():
             passwords = self.getPasswordsFromSearch(query)
         else:
@@ -64,12 +58,12 @@ class Plugin(TriggerQueryHandler):
         for password in passwords:
             name = password.split("/")[-1]
             results.append(
-                Item(
+                StandardItem(
                     id=password,
-                    icon=ICON,
                     text=name,
                     subtext=password,
-                    completion="pass %s" % password,
+                    iconUrls=self.iconUrls,
+                    inputActionText="pass %s" % password,
                     actions=[
                         Action("copy", "Copy", lambda pwd=password: runDetachedProcess(["pass", "--clip", pwd])),
                         Action("edit", "Edit", lambda pwd=password: runDetachedProcess(["pass", "edit", pwd])),
