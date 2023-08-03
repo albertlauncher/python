4bebd79cc38878c31f4b5d61aafba477c96f3b38
Manuel Schneider <manuelschneid3r@gmail.com>
2023-08-03 02:13:28 +0200

    [vbox] Interface v2.0

--- virtualbox/__init__.py
+++ virtualbox/__init__.py
@@ -5,8 +5,8 @@ from virtualbox.library import LockType, MachineState
 
 from albert import *
 
-md_iid = '1.0'
-md_version = "1.4"
+md_iid = '2.0'
+md_version = "1.5"
 md_name = "VirtualBox"
 md_description = "Manage your VirtualBox machines"
 md_license = "BSD-3"
@@ -23,47 +23,48 @@ def startVm(vm):
     except Exception as e:
         warning(str(e))
 
+
 def acpiPowerVm(vm):
     with vm.create_session(LockType.shared) as session:
         session.console.power_button()
+
 
 def stopVm(vm):
     with vm.create_session(LockType.shared) as session:
         session.console.power_down()
 
+
 def saveVm(vm):
     with vm.create_session(LockType.shared) as session:
         session.machine.save_state()
 
+
 def discardSavedVm(vm):
     with vm.create_session(LockType.shared) as session:
-        session.machine.discard_save_state(True);
+        session.machine.discard_save_state(True)
+
 
 def resumeVm(vm):
     with vm.create_session(LockType.shared) as session:
         session.console.resume()
 
+
 def pauseVm(vm):
     with vm.create_session(LockType.shared) as session:
         session.console.pause()
 
-class Plugin(TriggerQueryHandler):
-    iconUrls = ["xdg:virtualbox", ":unknown"]
 
-    def id(self):
-        return md_id
+class Plugin(PluginInstance, TriggerQueryHandler):
 
-    def name(self):
-        return md_name
-
-    def description(self):
-        return md_description
-
-    def synopsis(self):
-        return "<machine name>"
-
-    def defaultTrigger(self):
-        return "vbox "
+    def __init__(self):
+        TriggerQueryHandler.__init__(self,
+                                     id=md_id,
+                                     name=md_name,
+                                     description=md_description,
+                                     synopsis='<machine name>',
+                                     defaultTrigger='vbox ')
+        PluginInstance.__init__(self, extensions=[self])
+        self.iconUrls = ["xdg:virtualbox", ":unknown"]
 
     def handleTriggerQuery(self, query):
         items = []
@@ -71,26 +72,26 @@ class Plugin(TriggerQueryHandler):
         try:
             for vm in filter(lambda vm: pattern in vm.name.lower(), virtualbox.VirtualBox().machines):
                 actions = []
-                if vm.state == MachineState.powered_off or vm.state == MachineState.aborted:  #1 #4
+                if vm.state == MachineState.powered_off or vm.state == MachineState.aborted:  # 1 # 4
                     actions.append(Action("startvm", "Start virtual machine", lambda vm=vm: startVm(vm)))
-                if vm.state == MachineState.saved:  #2
+                if vm.state == MachineState.saved:  # 2
                     actions.append(Action("restorevm", "Start saved virtual machine", lambda vm=vm: startVm(vm)))
                     actions.append(Action("discardvm", "Discard saved state", lambda vm=vm: discardSavedVm(vm)))
-                if vm.state == MachineState.running:  #5
+                if vm.state == MachineState.running:  # 5
                     actions.append(Action("savevm", "Save virtual machine", lambda vm=vm: saveVm(vm)))
                     actions.append(Action("poweroffvm", "Power off via ACPI event (Power button)", lambda vm=vm: acpiPowerVm(vm)))
                     actions.append(Action("stopvm", "Turn off virtual machine", lambda vm=vm: stopVm(vm)))
                     actions.append(Action("pausevm", "Pause virtual machine", lambda vm=vm: pauseVm(vm)))
-                if vm.state == MachineState.paused:  #6
+                if vm.state == MachineState.paused:  # 6
                     actions.append(Action("resumevm", "Resume virtual machine", lambda vm=vm: resumeVm(vm)))
 
                 items.append(
-                    Item(
+                    StandardItem(
                         id=vm.__uuid__,
                         text=vm.name,
                         subtext="{vm.state}".format(vm=vm),
-                        completion=vm.name,
-                        icon=self.iconUrls,
+                        inputActionText=vm.name,
+                        iconUrls=self.iconUrls,
                         actions=actions
                     )
                 )
