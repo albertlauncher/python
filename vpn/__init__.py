c8725d89951a0a805198d320e735cab30f46105e
Manuel Schneider <manuelschneid3r@gmail.com>
2023-08-03 02:15:56 +0200

    [vpn] Interface v2.0

--- vpn/__init__.py
+++ vpn/__init__.py
@@ -1,9 +1,10 @@
-from albert import *
-from collections import namedtuple
 import subprocess
+from collections import namedtuple
 
-md_iid = '1.0'
-md_version = "1.3"
+from albert import *
+
+md_iid = '2.0'
+md_version = "1.4"
 md_id = "vpn"
 md_name = "VPN"
 md_description = "Manage NetworkManager VPN connections"
@@ -14,20 +15,17 @@ md_credits = ["@janeklb"]
 md_bin_dependencies = ["nmcli"]
 
 
-class Plugin(TriggerQueryHandler):
-
-    iconPath = ['xdg:network-wired']
+class Plugin(PluginInstance, TriggerQueryHandler):
 
     VPNConnection = namedtuple('VPNConnection', ['name', 'connected'])
 
-    def id(self):
-        return md_id
-
-    def name(self):
-        return md_name
-
-    def description(self):
-        return md_description
+    def __init__(self):
+        TriggerQueryHandler.__init__(self,
+                                     id=md_id,
+                                     name=md_name,
+                                     description=md_description,
+                                     defaultTrigger='vpn ')
+        PluginInstance.__init__(self, extensions=[self])
 
     def getVPNConnections(self):
         consStr = subprocess.check_output(
@@ -40,25 +38,24 @@ class Plugin(TriggerQueryHandler):
             if con[2] in ['vpn', 'wireguard']:
                 yield self.VPNConnection(name=con[0], connected=con[3] != '')
 
-
-    def buildItem(self,con):
+    @staticmethod
+    def buildItem(con):
         name = con.name
         command = 'down' if con.connected else 'up'
         text = f'Connect to {name}' if command == 'up' else f'Disconnect from {name}'
         commandline = ['nmcli', 'connection', command, 'id', name]
-        return Item(
+        return StandardItem(
             id=f'vpn-{command}-{name}',
             text=name,
             subtext=text,
-            icon=self.iconPath,
-            completion=name,
-            actions=[ Action("run",text=text, callable=lambda: runDetachedProcess(commandline)) ]
+            iconUrls=['xdg:network-wired'],
+            inputActionText=name,
+            actions=[Action("run", text=text, callable=lambda: runDetachedProcess(commandline))]
         )
 
-
-    def handleTriggerQuery(self,query):
+    def handleTriggerQuery(self, query):
         if query.isValid:
             connections = self.getVPNConnections()
             if query.string:
-                connections = [ con for con in connections if query.string.lower() in con.name.lower() ]
-            query.add([ self.buildItem(con) for con in connections ])
+                connections = [con for con in connections if query.string.lower() in con.name.lower()]
+            query.add([self.buildItem(con) for con in connections])
