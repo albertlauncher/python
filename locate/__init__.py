54e59966e8ebc12e4ac9100a0f54bcf5a2beb5c1
Manuel Schneider <manuelschneid3r@gmail.com>
2024-06-22 01:45:01 +0200

    [locate:1.10]
    - Min api 2.3

--- locate/__init__.py
+++ locate/__init__.py
@@ -13,12 +13,12 @@ from pathlib import Path
 
 from albert import *
 
-md_iid = '2.0'
-md_version = "1.9"
+md_iid = '2.3'
+md_version = "1.10"
 md_name = "Locate"
 md_description = "Find and open files using locate"
 md_license = "MIT"
-md_url = "https://github.com/albertlauncher/python/tree/master/locate"
+md_url = "https://github.com/albertlauncher/python/tree/main/locate"
 md_bin_dependencies = "locate"
 md_authors = "@manuelschneid3r"
 
@@ -26,13 +26,12 @@ md_authors = "@manuelschneid3r"
 class Plugin(PluginInstance, TriggerQueryHandler):
 
     def __init__(self):
-        TriggerQueryHandler.__init__(self,
-                                     id=md_id,
-                                     name=md_name,
-                                     description=md_description,
-                                     synopsis='<locate params>',
-                                     defaultTrigger="'")
-        PluginInstance.__init__(self, extensions=[self])
+        PluginInstance.__init__(self)
+        TriggerQueryHandler.__init__(
+            self, self.id, self.name, self.description,
+            synopsis='<locate params>',
+            defaultTrigger="'"
+        )
 
         self.iconUrls = [
             "xdg:preferences-system-search",
