2a503e63b4048a28e15e5d4b33bab4bd9ff93748
Manuel Schneider <manuelschneid3r@gmail.com>
2025-02-26 00:12:00 +0100

    [locate] v3

--- locate/__init__.py
+++ locate/__init__.py
@@ -13,8 +13,8 @@ from pathlib import Path
 
 from albert import *
 
-md_iid = '2.3'
-md_version = "1.10"
+md_iid = "3.0"
+md_version = "2.0"
 md_name = "Locate"
 md_description = "Find and open files using locate"
 md_license = "MIT"
@@ -27,11 +27,7 @@ class Plugin(PluginInstance, TriggerQueryHandler):
 
     def __init__(self):
         PluginInstance.__init__(self)
-        TriggerQueryHandler.__init__(
-            self, self.id, self.name, self.description,
-            synopsis='<locate params>',
-            defaultTrigger="'"
-        )
+        TriggerQueryHandler.__init__(self)
 
         self.iconUrls = [
             "xdg:preferences-system-search",
@@ -40,6 +36,12 @@ class Plugin(PluginInstance, TriggerQueryHandler):
             "xdg:text-x-generic",
             f"file:{Path(__file__).parent}/locate.svg"
         ]
+
+    def synopsis(self, query):
+        return "<locate params>"
+
+    def defaultTrigger(self):
+        return "'"
 
     def handleTriggerQuery(self, query):
         if len(query.string) > 2:
